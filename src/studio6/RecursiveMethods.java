2fb2f12a48e717299245113f2f75d3eb5afebc08
rtwin <rtwin@reagswin.dhcp.wustl.edu>
2025-03-05 17:21:08 -0600

    Worked on Studio 6.

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,11 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+		if (n == 0) {
+		return 0; 
+		} else {
+		return Math.pow((0.5), n) + geometricSum(n -1);		
+		}
 		
 	}
 
@@ -31,6 +35,16 @@ public class RecursiveMethods {
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
 		
 		// FIXME complete the recursive drawing
+		
+		if (radius < radiusMinimumDrawingThreshold) {
+			return;
+		} else {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter - radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter + radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter - radius, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter + radius, radius/3.0, radiusMinimumDrawingThreshold);
+		}
 	}
 	
 
@@ -43,10 +57,31 @@ public class RecursiveMethods {
 	public static int[] toReversed(int[] array) {
 		
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+		int index = array.length - 1;
+		
+		int[] reverseHelper = new int [array.length];
+		
+		if (index < 0) {
+			return reverseHelper;
+		} else {
+			return reverseHelper(index, array, reverseHelper);
+		}
 		
 	}
 
+	public static int[] reverseHelper(int index, int[] array, int[] reverseHelper) {
+		int[] reverse = new int [array.length];
+		
+		if (index < 0) {
+			return reverseHelper;
+		} else {
+			int mirrorIndex = array.length - 1 - index;
+			reverseHelper[index] = array[mirrorIndex];
+			return reverseHelper(index - 1, array, reverseHelper);
+		}
+	}
+	
+	
 	/**
 	 * This method uses recursion to compute the greatest common divisor
 	 * for the two input values
